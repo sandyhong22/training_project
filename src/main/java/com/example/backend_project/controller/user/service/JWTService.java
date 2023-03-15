a00452c3f30b03d9ee018ba0ac8ac84121eb110f
FA00255sandy.hong <sandy.hong@shoalter.com>
2023-03-15 14:56:07 +0800

    change jwt maven

--- src/main/java/com/example/backend_project/controller/user/service/JWTService.java
+++ src/main/java/com/example/backend_project/controller/user/service/JWTService.java
@@ -3,16 +3,12 @@ package com.example.backend_project.controller.user.service;
 import com.example.backend_project.entity.User;
 import com.example.backend_project.expection.AuthenticationException;
 import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKey;
 import javax.security.auth.message.AuthException;
 import javax.servlet.http.HttpServletRequest;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,41 +17,36 @@ import java.util.Map;
 @Slf4j
 public class JWTService {
     private static final String KEY = "eyJhbGciOiJIUzUxMiJ9.eyJuYW1lIjoic3NzIiwiZW1haWwiOiJ0ZXN0dCIsInVzZXJuYW1lIjoic2FuZHl5eSIsImV4cCI6MT";
-
+    
     public Claims decodeToken(HttpServletRequest request) throws AuthException {
-        String token =subJwtToken(request);
+        String token = subJwtToken(request);
         validateToken(token);
         return getAllClaims(token);
-
-
+        
+        
     }
+    
     public String generateToken(User user) {
-
+        
         Map<String, Object> claims = new HashMap<>();
         claims.put("username", user.getUsername());
         claims.put("name", user.getName());
         claims.put("email", user.getEmail());
-
-
+        
         Date expireDate =
-                //設定30min過期
-                new Date(System.currentTimeMillis() + 30 * 60 * 1000);
-
+                //set 24hr expire
+                new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000);
+        
         return Jwts.builder()
                 .setClaims(claims)
                 .setExpiration(expireDate)
-                .signWith(getKey(),SignatureAlgorithm.HS512)
+                .signWith(SignatureAlgorithm.HS512, KEY)
                 .compact();
     }
-
-    private SecretKey getKey() {
-        byte[] keyBytes = KEY.getBytes(StandardCharsets.UTF_8);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-
+    
     public void validateToken(String token) throws AuthException {
         try {
-            Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token);
+            Jwts.parser().setSigningKey(KEY).parseClaimsJws(token);
         } catch (SignatureException e) {
             throw new AuthException("Invalid JWT signature.");
         } catch (MalformedJwtException e) {
@@ -68,12 +59,11 @@ public class JWTService {
             throw new AuthException("JWT token compact of handler are invalid");
         }
     }
-
-
+    
     public String subJwtToken(HttpServletRequest request) throws AuthException {
         final String requestTokenHeader = request.getHeader("Authorization");
-
-
+        
+        
         if (StringUtils.isNotEmpty(requestTokenHeader) && requestTokenHeader.startsWith("Bearer")) {
             return requestTokenHeader.substring(7);
         } else {
@@ -81,8 +71,8 @@ public class JWTService {
             throw new AuthenticationException("INVALID_HEADER");
         }
     }
-
+    
     public Claims getAllClaims(String token) {
-        return Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token).getBody();
+        return Jwts.parser().setSigningKey(KEY).parseClaimsJws(token).getBody();
     }
 }
