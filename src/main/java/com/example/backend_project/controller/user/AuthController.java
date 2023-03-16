2dd7109d943b128f39717001d018ef744a780d8a
Sandy.Hong <sandy.hong@shoalter.com>
2023-03-16 18:41:24 +0800

    fix: change login and register api to AuthController

---
+++ src/main/java/com/example/backend_project/controller/user/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.backend_project.controller.user;
+
+import com.example.backend_project.controller.user.dto.reponse.LoginResponse;
+import com.example.backend_project.controller.user.dto.request.LoginRequest;
+import com.example.backend_project.controller.user.dto.request.UserRegisterRequest;
+import com.example.backend_project.controller.user.service.LoginService;
+import com.example.backend_project.controller.user.service.RegisterService;
+import com.example.backend_project.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+    private final LoginService loginService;
+    private final RegisterService registerService;
+    
+    @PostMapping("/login")
+    public ResponseDto<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        return ResponseDto.success(loginService.login(loginRequest));
+    }
+    
+    @PostMapping("/register")
+    public ResponseDto<LoginResponse> register(@Valid @RequestBody UserRegisterRequest userRegisterRequest) {
+        return ResponseDto.success(registerService.register(userRegisterRequest));
+    }
+}
--- src/main/java/com/example/backend_project/controller/user/UserController.java
+++ src/main/java/com/example/backend_project/controller/user/UserController.java
@@ -1,9 +1,6 @@
 package com.example.backend_project.controller.user;
 
-import com.example.backend_project.controller.user.dto.reponse.LoginResponse;
 import com.example.backend_project.controller.user.dto.reponse.UserResponse;
-import com.example.backend_project.controller.user.dto.request.LoginRequest;
-import com.example.backend_project.controller.user.dto.request.UserRegisterRequest;
 import com.example.backend_project.controller.user.service.FindAllUserService;
 import com.example.backend_project.controller.user.service.LoginService;
 import com.example.backend_project.controller.user.service.RegisterService;
@@ -12,13 +9,20 @@ import com.example.backend_project.dto.ResponseDto;
 import com.example.backend_project.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Valid
+@RequestMapping("/account")
 public class UserController {
     private final FindAllUserService accountService;
     
@@ -28,25 +32,15 @@ public class UserController {
     
     private final LoginService loginService;
     
-    @GetMapping("/account")
+    @GetMapping()
     public ResponseDto<List<User>> getAllAccount() {
         return ResponseDto.success(accountService.getAllAccount());
     }
     
     
-    @PostMapping("/register")
-    public ResponseDto<User> register(@RequestBody UserRegisterRequest userRegisterRequest) {
-        return ResponseDto.success(registerService.register(userRegisterRequest));
-    }
-    
-    
-    @GetMapping("/account/username/{username}")
+    @GetMapping("/username/{username}")
     public ResponseDto<UserResponse> findByUsername(@PathVariable("username") String username) {
         return ResponseDto.success(userFindByUsername.findByUsername(username));
     }
     
-    @PostMapping("/login")
-    public ResponseDto<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-        return ResponseDto.success(loginService.login(loginRequest));
-    }
 }
