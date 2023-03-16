cf0dd95d831e41eeabd7e01d798dd04f5cb0b9c8
Sandy.Hong <sandy.hong@shoalter.com>
2023-03-16 18:43:22 +0800

    feat : User entity implements UserDetails

--- src/main/java/com/example/backend_project/entity/User.java
+++ src/main/java/com/example/backend_project/entity/User.java
@@ -1,14 +1,19 @@
 package com.example.backend_project.entity;
 
+import com.example.backend_project.enums.UserRole;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Data
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,21 +21,53 @@ public class User {
     private Long id;
 
     @Column(name = "username")
+    @NotBlank
     private String username;
 
     @Column(name = "email")
+    @NotBlank
     private String email;
 
     @Column(name = "name")
+    @NotBlank
     private String name;
 
     @Column(name = "password")
+    @NotBlank
     private String password;
+    
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 
     @Column(name = "created_date")
     private LocalDateTime createdDate;
 
     @Column(name="last_modified_date")
     private LocalDateTime lastModifiedDate;
-
+    
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
