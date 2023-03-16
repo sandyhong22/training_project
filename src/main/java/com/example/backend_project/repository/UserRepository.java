22528ed9171071cae74658541d3b03d05d0cbeef
Sandy.Hong <sandy.hong@shoalter.com>
2023-03-16 18:35:14 +0800

    fix: change findByUsername to Optional

--- src/main/java/com/example/backend_project/repository/UserRepository.java
+++ src/main/java/com/example/backend_project/repository/UserRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Transactional
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsername(String username);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    
     Boolean existsByUsername(String username);
 }
